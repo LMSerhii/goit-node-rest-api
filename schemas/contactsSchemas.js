0b58ebf4304f2eb38d5660db2f9e686f64454960
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-04-01 12:03:52 +0300

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,10 +4,12 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
-});
+}).options({ abortEarly: false });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email(),
   phone: Joi.string(),
-}).min(1);
+})
+  .min(1)
+  .options({ abortEarly: false });
